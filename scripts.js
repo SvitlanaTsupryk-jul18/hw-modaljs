f6238ae2ad74fd55b8b66a6030d3e018d6941fe4
Svet <stsupryk@gmail.com>
2018-11-27 01:10:43 +0200

    done homework

--- scripts.js
+++ scripts.js
@@ -33,61 +33,22 @@
         document.querySelector(".js-mobile-menu").appendChild(mobileMenu);
     }
 
-    // function modalWindow() {
-    //   var className = '.js-modal';
-    //   var activeClass = 'open';
-    //   var modal = document.querySelector(className);
-    //   // null
-    //   // Node
-    //   if (!modal) return;
-
-    //   var modalTrigger = document.querySelector(className + '-trigger');
-    //   var modalClose = document.querySelector(className + '-close');
-
-    //   modalTrigger.addEventListener('click', function(e) {
-    //     e.preventDefault();
-    //     toggleModal();
-    //     // вы же знаете на какую кнопку кликнули?
-    //   });
-
-    //   modalClose.addEventListener('click', function (e) {
-    //     e.preventDefault();
-    //     toggleModal();
-    //   });
-
-    //   function toggleModal() {
-    //     document.querySelector('body').classList.toggle('modal-open');
-    //     // скрой все секции в модалке
-    //     // покажи конкретную секцию в модалке
-    //     modal.classList.toggle(activeClass);
-    //   }
-
-    // }
-
-
-    function modal() {
+    function modalWindow1() {
         var className = '.js-modal';
         var activeClass = 'open';
         var modal = document.querySelector(className);
-
         // null
         // Node
         if (!modal) return;
 
-        var modalTrigger = document.querySelectorAll(className + '-trigger'); // кнопка
+        var modalTrigger = document.querySelector(className + '-trigger');
         var modalClose = document.querySelector(className + '-close');
-        var content = document.querySelectorAll('p[data-item]');
 
-
-        modalTrigger.forEach(function(item, i, arr) {
-            modalTrigger[i].addEventListener('click', function(e) {
-                e.preventDefault();
-                toggleModal();
-                // вы же знаете на какую кнопку кликнули?
-                console.log(this.dataset.item);
-            });
+        modalTrigger.addEventListener('click', function(e) {
+            e.preventDefault();
+            toggleModal();
+            // вы же знаете на какую кнопку кликнули?
         });
-
 
         modalClose.addEventListener('click', function(e) {
             e.preventDefault();
@@ -95,23 +56,57 @@
         });
 
         function toggleModal() {
-            //////!!!!!!!!!ничего не передается!!!!
-            console.log(this.dataset.item);
-
             document.querySelector('body').classList.toggle('modal-open');
             // скрой все секции в модалке
-            var cont = document.querySelectorAll("div.js-modal>div");
-            cont.forEach(function(item, i, arr) {
-                cont[i].style.display = 'none';
-                //console.log(cont[i]);
-            });
             // покажи конкретную секцию в модалке
-            let c = 'data-item =' + '"' + this.dataset.item + '"';
-            var text = content.querySelector("[" + c + "]");
-            modal.appendChild(text);
             modal.classList.toggle(activeClass);
         }
 
     }
 
+
+    function modal() {
+        var className = '.js-modal';
+        var activeClass = 'open';
+        var modal = document.querySelector(className);
+        // null
+        // Node
+        if (!modal) return;
+        var modalTrigger = document.querySelectorAll(className + '-trigger'); // кнопка
+        var modalClose = document.querySelector(className + '-close');
+
+        modalTrigger.forEach(function(item, i, arr) {
+            modalTrigger[i].addEventListener('click', function(e) {
+                e.preventDefault();
+                // вы же знаете на какую кнопку кликнули?
+                //console.log(this.dataset.item);
+                toggleModal.call(this); // вызывает функцию с указанным значением this 
+            });
+        });
+
+        modalClose.addEventListener('click', function(e) {
+            e.preventDefault();
+            modal.classList.toggle(activeClass);
+        });
+
+        function toggleModal() {
+            //console.log(this.dataset.item);
+            document.querySelector('body').classList.toggle('modal-open');
+            // скрой все секции в модалке
+            var cont = modal.querySelectorAll("section");
+            cont.forEach(function(item, i, arr) {
+                cont[i].style.display = 'none';
+            });
+            // покажи конкретную секцию в модалке
+            var section = modal.querySelectorAll('[data-item]');
+            let c = this.dataset.item;
+            for (var i = 0; i < section.length; i++) {
+                if (section[i].dataset.item == c) {
+                    section[i].style.display = 'block';
+                    section[i].style.color = 'white';
+                }
+            }
+            modal.classList.toggle(activeClass);
+        }
+    }
 })();
